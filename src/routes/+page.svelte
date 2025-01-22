2bca73eced9123add010b462f119125e0a3218a8
knyghtw <tmbwpva@protonmail.com>
2025-01-23 03:01:53 +0700

    FE Update

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,4 +1,6 @@
 <script>
+  import { fly } from "svelte/transition";
+
   import {
     Button,
     Dropdown,
@@ -11,6 +13,9 @@
     TableHeadCell,
     Tabs,
     TabItem,
+    Modal,
+    Label,
+    Input,
   } from "flowbite-svelte";
 
   import {
@@ -19,58 +24,63 @@
     sendNotification,
   } from "@tauri-apps/plugin-notification";
 
-  import { PlusOutline, BellSolid, EyeSolid } from "flowbite-svelte-icons";
+  import {
+    PlusOutline,
+    BellSolid,
+    EyeSolid,
+    ChevronDownOutline,
+    PenSolid,
+    TrashBinSolid,
+  } from "flowbite-svelte-icons";
 
   let notificationGranted = false;
+  let isAddingPBF = false;
+  let clickCreateDataModal = false;
+  let isDataObat = true;
+  let isStokObat = false;
+  let isTglExp = false;
+  let isStockAlert = false;
 
-  async function setupNotification() {
-    notificationGranted = await isPermissionGranted();
+  // async function setupNotification() {
+  //   notificationGranted = await isPermissionGranted();
 
-    if (!notificationGranted) {
-      const permission = await requestPermission();
-      notificationGranted = permission === "granted";
-    }
+  //   if (!notificationGranted) {
+  //     const permission = await requestPermission();
+  //     notificationGranted = permission === "granted";
+  //   }
 
-    if (notificationGranted) {
-      sendNotification({
-        title: "Notifikasi",
-        body: "Notifikasi berhasil diaktifkan",        
-      });
-    }
-  }
+  //   if (notificationGranted) {
+  //     sendNotification({
+  //       title: "Notifikasi",
+  //       body: "Notifikasi berhasil diaktifkan",
+  //     });
+  //   }
+  // }
 
-  setupNotification();
+  // setupNotification();
 
   let searchTermBarang = "";
   let searchTermStok = "";
+  let searchStockAlert = "";
 
   let items_barang = [
     {
       id_obat: 1,
       nama_obat: "Paracetamol 500mg",
-      harga_beli: 2000,
-      harga_jual: 2200,
-      pbf: "PT BioHealth",
       satuan: "Tablet",
-      diskon: 10,
+      jml_stok: 10,
     },
     {
       id_obat: 2,
       nama_obat: "Amoxicillin",
-      harga_beli: 3500,
-      harga_jual: 3850,
-      pbf: "PT AMS",
       satuan: "Kapsul",
-      diskon: 2,
+      jml_stok: 2,
     },
     {
       id_obat: 3,
       nama_obat: "Ibuprofen",
-      harga_beli: 7000,
-      harga_jual: 7700,
-      pbf: "PT ABC",
       satuan: "Tablet",
-      diskon: 5,
+      jml_stok: 5,
     },
   ];
 
@@ -80,6 +90,8 @@
       id_obat: 1,
       nama_obat: "Paracetamol 500mg",
       no_batch: "LKASD12",
+      harga_beli: 2000,
+      harga_jual: 2200,
       tanggal_expired: "12 Maret 2025",
       jumlah: 100,
     },
@@ -88,6 +100,8 @@
       id_obat: 2,
       nama_obat: "Amoxicillin",
       no_batch: "AKDSA21",
+      harga_beli: 3000,
+      harga_jual: 3300,
       tanggal_expired: "13 Maret 2025",
       jumlah: 50,
     },
@@ -96,6 +110,8 @@
       id_obat: 3,
       nama_obat: "Ibuprofen",
       no_batch: "KDASA11",
+      harga_beli: 4000,
+      harga_jual: 4400,
       tanggal_expired: "14 Maret 2025",
       jumlah: 75,
     },
@@ -103,6 +119,137 @@
 </script>
 
 <main class="m-4">
+  <Modal bind:open={clickCreateDataModal} autoclose={false} outsideclose>
+    {#if !isAddingPBF}
+      <form
+        in:fly={{ x: 50, duration: 300 }}
+        out:fly={{ x: -50, duration: 300 }}
+        class="flex flex-col space-y-4"
+        action="#"
+      >
+        <h3 class="text-xl font-medium text-gray-900 dark:text-white">
+          Tambah Data
+        </h3>
+        <hr />
+        <Label class="space-y-2">
+          <span class="text-gray-900">Nama Obat</span>
+          <Input
+            type="text"
+            name="nama_obat"
+            placeholder="Paracetamol"
+            class="font-normal"
+            required
+          />
+        </Label>
+        <Label class="space-y-2">
+          <span class="text-gray-900">Harga Beli</span>
+          <Input
+            type="number"
+            name="harga_beli"
+            placeholder="10000"
+            class="font-normal"
+            required
+          />
+        </Label>
+        <Label class="space-y-2">
+          <span class="text-gray-900">Harga Jual</span>
+          <Input
+            type="number"
+            name="harga_jual"
+            placeholder="11000"
+            class="font-normal"
+            required
+          />
+        </Label>
+        <Label class="space-y-2 flex flex-col">
+          <span class="text-gray-900">PBF</span>
+          <Button color="alternative" class="hover:text-black text-black"
+            >Pilih PBF disini<ChevronDownOutline
+              class="w-6 h-6 ms-2 text-black dark:text-white"
+            /></Button
+          >
+          <Dropdown class="w-48 overflow-y-auto py-1 h-48 ">
+            <DropdownItem
+              class="flex items-center text-base font-semibold gap-2"
+            >
+              Leslie Livingston
+            </DropdownItem>
+            <a
+              slot="footer"
+              href="/"
+              class="flex justify-center px-3 py-2 -mb-1 text-sm font-medium text-primary-600 bg-gray-50 hover:bg-gray-100 dark:bg-gray-700 dark:hover:bg-gray-600 dark:text-primary-500 hover:underline"
+              on:click={() => (isAddingPBF = true)}
+            >
+              Tambah PBF Baru
+            </a>
+          </Dropdown>
+        </Label>
+        <Label class="space-y-2">
+          <span class="text-gray-900">Satuan</span>
+          <Input
+            type="text"
+            name="satuan"
+            placeholder="Tablet"
+            class="font-normal"
+            required
+          />
+        </Label>
+        <Label class="space-y-2">
+          <span class="text-gray-900">Diskon (%)</span>
+          <Input
+            type="number"
+            name="diskon"
+            placeholder="10"
+            class="font-normal"
+            required
+          />
+        </Label>
+        <hr />
+        <Button
+          on:click={() => {
+            alert('Handle "success"');
+            clickCreateDataModal = false;
+          }}>Tambah</Button
+        >
+      </form>
+    {:else}
+      <form
+        in:fly={{ x: -50, duration: 300 }}
+        out:fly={{ x: 50, duration: 300 }}
+        class="flex flex-col space-y-4"
+        on:submit|preventDefault={() => {
+          isAddingPBF = false;
+          clickCreateDataModal = false;
+        }}
+      >
+        <h3 class="text-xl font-medium text-gray-900 dark:text-white">
+          Tambah Data PBF
+        </h3>
+        <hr />
+        <Label class="space-y-2">
+          <span class="text-gray-900">Nama PBF</span>
+          <Input
+            type="text"
+            name="pbf"
+            placeholder="PT ABC"
+            class="font-normal"
+            required
+          />
+        </Label>
+        <div class="flex flex-row justify-between space-x-4">
+          <Button class="flex flex-1" type="submit">Simpan</Button>
+          <Button
+            class="flex flex-1"
+            type="button"
+            color="alternative"
+            on:click={() => (isAddingPBF = false)}
+          >
+            Kembali
+          </Button>
+        </div>
+      </form>
+    {/if}
+  </Modal>
   <div class="flex justify-between">
     <h1 class="text-3xl font-bold">PharmAssist</h1>
     <div
@@ -185,7 +332,16 @@
     </Dropdown>
   </div>
   <Tabs tabStyle="underline">
-    <TabItem open title="Data Obat">
+    <TabItem
+      open={isDataObat}
+      title="Data Obat"
+      on:click={() => {
+        isDataObat = true;
+        isStokObat = false;
+        isTglExp = false;
+        isStockAlert = false;
+      }}
+    >
       <div class="flex space-x-4 mb-4 justify-between">
         <input
           type="text"
@@ -193,7 +349,7 @@
           placeholder="Cari data obat..."
           bind:value={searchTermBarang}
         />
-        <Button>
+        <Button on:click={() => (clickCreateDataModal = true)}>
           <PlusOutline class="w-5 h-5 me-2" />Tambah Data
         </Button>
       </div>
@@ -202,11 +358,9 @@
           <TableHead>
             <TableHeadCell>No</TableHeadCell>
             <TableHeadCell>Nama Obat</TableHeadCell>
-            <TableHeadCell>Harga Beli</TableHeadCell>
-            <TableHeadCell>Harga Jual</TableHeadCell>
-            <TableHeadCell>PBF</TableHeadCell>
             <TableHeadCell>Satuan</TableHeadCell>
-            <TableHeadCell>Diskon</TableHeadCell>
+            <TableHeadCell>Jumlah Stok</TableHeadCell>
+            <TableHeadCell>Aksi</TableHeadCell>
           </TableHead>
           <TableBody tableBodyClass="divide-y">
             {#each items_barang.filter((item) => item.nama_obat
@@ -215,18 +369,43 @@
               <TableBodyRow>
                 <TableBodyCell>{item.id_obat}</TableBodyCell>
                 <TableBodyCell>{item.nama_obat}</TableBodyCell>
-                <TableBodyCell>{item.harga_beli}</TableBodyCell>
-                <TableBodyCell>{item.harga_jual}</TableBodyCell>
-                <TableBodyCell>{item.pbf}</TableBodyCell>
                 <TableBodyCell>{item.satuan}</TableBodyCell>
-                <TableBodyCell>{item.diskon}</TableBodyCell>
+                <TableBodyCell>{item.jml_stok}</TableBodyCell>
+                <TableBodyCell>
+                  <div class="flex space-x-4">
+                    <Button
+                      on:click={() => {
+                        isStokObat = true;
+                        isDataObat = false;
+                        searchTermStok = item.nama_obat;
+                      }}
+                      color="blue"
+                      pill={true}
+                      class="!p-2"><EyeSolid class="w-6 h-6" /></Button
+                    ><Button color="yellow" pill={true} class="!p-2"
+                      ><PenSolid class="w-6 h-6" /></Button
+                    ><Button color="red" pill={true} class="!p-2"
+                      ><TrashBinSolid class="w-6 h-6" /></Button
+                    >
+                  </div>
+                </TableBodyCell>
               </TableBodyRow>
             {/each}
           </TableBody>
         </Table>
       </p>
     </TabItem>
-    <TabItem title="Stok Obat">
+    <TabItem
+      open={isStokObat}
+      title="Stok Obat"
+      on:click={() => {
+        isDataObat = false;
+        isStokObat = true;
+        isTglExp = false;
+        isStockAlert = false;
+        searchTermStok = "";
+      }}
+    >
       <div class="flex space-x-4 mb-4 justify-between">
         <input
           type="text"
@@ -244,8 +423,10 @@
           <TableHeadCell>No</TableHeadCell>
           <TableHeadCell>Nama Obat</TableHeadCell>
           <TableHeadCell>No Batch</TableHeadCell>
+          <TableHeadCell>Harga Beli</TableHeadCell>
+          <TableHeadCell>Harga Jual</TableHeadCell>
           <TableHeadCell>Tanggal Expired</TableHeadCell>
-          <TableHeadCell>Jumlah</TableHeadCell>
+          <TableHeadCell>Jumlah Stok</TableHeadCell>
         </TableHead>
         <TableBody tableBodyClass="divide-y">
           {#each items_stok.filter((item) => item.nama_obat
@@ -255,6 +436,8 @@
               <TableBodyCell>{item.id_stok}</TableBodyCell>
               <TableBodyCell>{item.nama_obat}</TableBodyCell>
               <TableBodyCell>{item.no_batch}</TableBodyCell>
+              <TableBodyCell>{item.harga_beli}</TableBodyCell>
+              <TableBodyCell>{item.harga_jual}</TableBodyCell>
               <TableBodyCell>{item.tanggal_expired}</TableBodyCell>
               <TableBodyCell>{item.jumlah}</TableBodyCell>
             </TableBodyRow>
@@ -262,13 +445,32 @@
         </TableBody>
       </Table>
     </TabItem>
-    <TabItem title="Tanggal Expired">
+    <TabItem
+      open={isTglExp}
+      title="Tanggal Expired"
+      on:click={() => {
+        isDataObat = false;
+        isStokObat = false;
+        isTglExp = true;
+        isStockAlert = false;
+      }}
+    >
       <p class="text-sm text-gray-500 dark:text-gray-400">
         <b>Tanggal Expired:</b>
         Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor
         incididunt ut labore et dolore magna aliqua.
       </p>
     </TabItem>
+    <TabItem
+      open={isStockAlert}
+      title="Stok < 25"
+      on:click={() => {
+        isDataObat = false;
+        isStokObat = false;
+        isTglExp = false;
+        isStockAlert = true;
+      }}
+    ></TabItem>
   </Tabs>
 </main>
 
