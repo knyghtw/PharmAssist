60996baa902c9295036ee3cb3081ba12453427dc
knyghtw <tmbwpva@protonmail.com>
2025-02-01 15:08:21 +0700

    Add Select, Add Create Data Obat

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -2,7 +2,7 @@
   import { fly } from "svelte/transition";
 
   import {
-    Button,    
+    Button,
     Table,
     TableBody,
     TableBodyCell,
@@ -79,7 +79,7 @@
       id_obat: 0,
       nama_obat: "",
       satuan: "",
-      jml_stok: null,
+      jml_stok: 0,
     },
   ]);
 
@@ -104,6 +104,8 @@
   ]);
 
   let nama_pbf = $state("");
+  let nama_obat = $state("");
+  let satuan = $state("");
 
   async function getItems() {
     try {
@@ -141,6 +143,35 @@
       if (result) {
         alert("Added PBF: " + nama_pbf);
         console.log("Added PBF: " + nama_pbf);
+        getItems();
+        return { success: true, message: "Data PBF berhasil ditambahkan" };
+      }
+    } catch (error) {
+      alert(error);
+      console.log(error);
+      return { success: false, message: error };
+    }
+  }
+
+  async function setDataObat() {
+    try {
+      const db = await Database.load("sqlite:test.db");
+      const existingData = await db.select(
+        "SELECT nama_obat FROM barang WHERE nama_obat = $1",
+        [nama_obat]
+      );
+
+      if (existingData.length > 0) {
+        throw new Error(`PBF dengan nama "${nama_pbf}" sudah ada`);
+      }
+
+      const result = await db.execute(
+        "INSERT INTO barang (nama_obat, satuan, total_stok) VALUES ($1, $2, 0)",
+        [nama_obat, satuan]
+      );
+      if (result) {
+        alert("Added PBF: " + nama_obat);
+        console.log("Added PBF: " + nama_obat);
         getItems();
         return { success: true, message: "Data PBF berhasil ditambahkan" };
       }
@@ -311,7 +342,14 @@
     </form>
   </Modal>
   <Modal bind:open={clickCreateDataModal} autoclose={false} outsideclose>
-    <form class="flex flex-col space-y-4" action="#">
+    <form
+      class="flex flex-col space-y-4"
+      onsubmit={() => {
+        setDataObat();
+        clickCreateDataModal = false;
+        selectedPBF = "Pilih PBF disini";
+      }}
+    >
       <h3 class="text-xl font-medium text-gray-900 dark:text-white">
         Tambah Data
       </h3>
@@ -319,6 +357,7 @@
       <Label class="space-y-2">
         <span class="text-gray-900">Nama Obat</span>
         <Input
+          bind:value={nama_obat}
           type="text"
           name="nama_obat"
           placeholder="Paracetamol"
@@ -326,7 +365,7 @@
           required
         />
       </Label>
-      <Label class="space-y-2">
+      <!-- <Label class="space-y-2">
         <span class="text-gray-900">Harga Beli</span>
         <Input
           type="number"
@@ -348,50 +387,16 @@
       </Label>
       <Label class="space-y-2 flex flex-col">
         <span class="text-gray-900">PBF</span>
-        <Button
-          color="alternative"
-          class="hover:text-black text-black"          
-          >{selectedPBF}<ChevronDownOutline
-            class="w-6 h-6 ms-2 text-black dark:text-white"
-          /></Button
-        >
-        <!-- <Dropdown
-          bind:open={dropdownPBFOpen}
-          placement="bottom"
-          class="w-48 overflow-y-auto py-1 h-48 "
-        >
+        <Select bind:value={selectedPBF} class="w-full">
           {#each items_pbf as item_pbf}
-            <DropdownItem
-              class="flex items-center text-base font-semibold gap-2"
-              on:click={() => {
-                selectedPBF = item_pbf.nama_pbf;
-                dropdownPBFOpen = false;                
-              }}
-            >
-              {item_pbf.nama_pbf}
-            </DropdownItem>
+            <option value={item_pbf.nama_pbf}>{item_pbf.nama_pbf}</option>
           {/each}
-          <a
-            slot="footer"
-            href="/"
-            class="flex justify-center px-3 py-2 -mb-1 text-sm font-medium text-primary-600 bg-gray-50 hover:bg-gray-100 dark:bg-gray-700 dark:hover:bg-gray-600 dark:text-primary-500 hover:underline"
-            onclick={() => {
-              isDataObat = false;
-              isPBF = true;
-              isFromDataObat = true;
-              clickCreateDataModal = false;
-              clickCreatePBFModal = true;
-            }}
-          >
-            Tambah PBF Baru
-          </a>
-        </Dropdown> -->
-        
-        
-      </Label>
+        </Select>
+      </Label> -->
       <Label class="space-y-2">
         <span class="text-gray-900">Satuan</span>
         <Input
+          bind:value={satuan}
           type="text"
           name="satuan"
           placeholder="Tablet"
@@ -399,7 +404,7 @@
           required
         />
       </Label>
-      <Label class="space-y-2">
+      <!-- <Label class="space-y-2">
         <span class="text-gray-900">Diskon (%)</span>
         <Input
           type="number"
@@ -408,11 +413,11 @@
           class="font-normal"
           required
         />
-      </Label>
+      </Label> -->
       <hr />
       <Button
         on:click={() => {
-          alert('Handle "success"');
+          setDataObat();
           clickCreateDataModal = false;
         }}>Tambah</Button
       >
@@ -431,73 +436,6 @@
         ></div>
       </div>
     </div>
-    <!-- <Dropdown
-      placement="left"
-      triggeredBy="#bell"
-      class="w-full max-w-sm rounded divide-y divide-gray-100 shadow dark:bg-gray-800 dark:divide-gray-700"
-    >
-      <div slot="header" class="text-center py-2 font-bold">
-        Obat Mendekati Expired
-      </div>
-      <DropdownItem class="flex space-x-4 rtl:space-x-reverse">
-        <div class="ps-3 w-full">
-          <div class="text-gray-500 text-sm mb-1.5 dark:text-gray-400">
-            No. Batch: <span class="font-semibold text-gray-900 dark:text-white"
-              >LKASD12</span
-            ><br />
-            Item:
-            <span class="font-semibold text-gray-900 dark:text-white"
-              >Paracetamol 500mg</span
-            >
-          </div>
-          <div class="text-xs text-primary-600 dark:text-primary-500">
-            Exp: 31 Februari 2025
-          </div>
-        </div>
-      </DropdownItem>
-      <DropdownItem class="flex space-x-4 rtl:space-x-reverse">
-        <div class="ps-3 w-full">
-          <div class="text-gray-500 text-sm mb-1.5 dark:text-gray-400">
-            No. Batch: <span class="font-semibold text-gray-900 dark:text-white"
-              >LKASD12</span
-            ><br />
-            Item:
-            <span class="font-semibold text-gray-900 dark:text-white"
-              >Dexamethasone</span
-            >
-          </div>
-          <div class="text-xs text-primary-600 dark:text-primary-500">
-            Exp: 22 April 2025
-          </div>
-        </div>
-      </DropdownItem>
-      <DropdownItem class="flex space-x-4 rtl:space-x-reverse">
-        <div class="ps-3 w-full">
-          <div class="text-gray-500 text-sm mb-1.5 dark:text-gray-400">
-            No. Batch: <span class="font-semibold text-gray-900 dark:text-white"
-              >ASJ1231</span
-            ><br />
-            Item:
-            <span class="font-semibold text-gray-900 dark:text-white"
-              >Ibuprofen</span
-            >
-          </div>
-          <div class="text-xs text-primary-600 dark:text-primary-500">
-            Exp: 12 Maret 2025
-          </div>
-        </div>
-      </DropdownItem>
-      <a
-        slot="footer"
-        href="/"
-        class="block py-2 -my-1 text-sm font-medium text-center text-gray-900 bg-gray-50 hover:bg-gray-100 dark:bg-gray-800 dark:hover:bg-gray-700 dark:text-white"
-      >
-        <div class="inline-flex items-center">
-          <EyeSolid class="me-2 w-4 h-4 text-gray-500 dark:text-gray-400" />
-          View all
-        </div>
-      </a>
-    </Dropdown> -->
   </div>
   <Tabs tabStyle="underline">
     <TabItem
