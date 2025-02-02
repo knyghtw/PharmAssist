2958bccda8e4fe62cf947d5ae2da2574946783d5
knyghtw <tmbwpva@protonmail.com>
2025-02-03 01:23:59 +0700

    alter table column

--- src-tauri/src/lib.rs
+++ src-tauri/src/lib.rs
@@ -42,6 +42,40 @@ pub fn run() {
                  CREATE UNIQUE INDEX idx_nama_pbf ON pbf(nama_pbf);
                  CREATE UNIQUE INDEX idx_batch_barang_pbf ON stok_obat(no_batch, id_barang, id_pbf);",
             kind: MigrationKind::Up,
+        },
+        Migration {
+            version: 3,
+            description: "drop_existing_tables",
+            sql: "DROP TABLE IF EXISTS stok_obat;
+                 DROP TABLE IF EXISTS barang;
+                 DROP TABLE IF EXISTS pbf;",
+            kind: MigrationKind::Up,
+        },
+        Migration {
+            version: 4,
+            description: "create_new_tables",
+            sql: "CREATE TABLE IF NOT EXISTS barang (
+                    id_barang INTEGER PRIMARY KEY,
+                    nama_barang VARCHAR UNIQUE,
+                    satuan VARCHAR
+                );
+                CREATE TABLE IF NOT EXISTS pbf (
+                    id_pbf INTEGER PRIMARY KEY,
+                    nama_pbf VARCHAR UNIQUE
+                );
+                CREATE TABLE IF NOT EXISTS stok_obat (
+                    id_stok INTEGER PRIMARY KEY,
+                    id_barang INTEGER UNIQUE,
+                    id_pbf INTEGER UNIQUE,
+                    no_batch VARCHAR UNIQUE,
+                    harga_beli_per_satuan DECIMAL,
+                    harga_jual_per_satuan DECIMAL,
+                    tanggal_expired DATE,
+                    jumlah_stok INTEGER,
+                    FOREIGN KEY (id_barang) REFERENCES barang(id_barang),
+                    FOREIGN KEY (id_pbf) REFERENCES pbf(id_pbf)
+                );",
+            kind: MigrationKind::Up,
         },        
     ];
 
